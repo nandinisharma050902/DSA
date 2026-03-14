c29d9d80fa71ebc26cd62c4be11cf36e1623222d
Nandini Sharma <nandinisharma050902@gmail.com>
2026-03-15 02:49:04 +0530

    Add files via upload

---
+++ maxconsone.java
@@ -0,0 +1,25 @@
+// class Solution {
+//     public int findMaxConsecutiveOnes(int[] nums) {
+//         int cnt = 0;
+//         int maxi = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] == 1) {
+//                 cnt++;
+//             } else {
+//                 cnt = 0;
+//             }
+//             maxi = Math.max(maxi, cnt);
+//         }
+//         return maxi;
+//     }
+// }
+
+// public class maxconsone {
+//     public static void main(String[] args) {
+//         int[] nums = {1, 1, 0, 1, 1, 1};
+//         Solution obj = new Solution();
+//         int ans = obj.findMaxConsecutiveOnes(nums);
+//         System.out.println("The maximum consecutive 1's are " + ans);
+//     }
+// } //TC: o(n) and SC: o(1)
+
