f58453858f7730803dd26121ab77e371617e6a22
Nandini Sharma <nandinisharma050902@gmail.com>
2026-03-14 23:06:37 +0530

    Add files via upload

---
+++ duplicates.java
@@ -0,0 +1,61 @@
+//brute force approach
+// import java.util.*;
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         HashSet<Integer> seen = new HashSet<>();
+//         int index = 0;
+//         for (int num : nums) {
+//             if (!seen.contains(num)) {
+//                 seen.add(num);
+//                 nums[index] = num;
+//                 index++;
+//             }
+//         }
+//         return index;
+//     }
+// }
+
+// public class duplicates{
+//     public static void main(String[] args) {
+//         int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+//         Solution sol = new Solution();
+//         int k = sol.removeDuplicates(nums);
+
+//         System.out.println("k = " + k);
+//         System.out.print("Array after removing duplicates: ");
+//         for (int i = 0; i < k; i++) {
+//             System.out.print(nums[i] + " ");
+//         }
+//     }
+// } //TC: o(n) and SC: o(n)
+
+//optimal approach
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
+
+public class duplicates {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        Solution sol = new Solution();
+        int k = sol.removeDuplicates(nums);
+
+        System.out.println("Unique count = " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int x = 0; x < k; x++) {
+            System.out.print(nums[x] + " ");
+        }
+    }
+}//TC: o(n) and SC: o(1)
