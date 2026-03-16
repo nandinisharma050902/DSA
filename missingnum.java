16a5c5698cf938afde8134d0935049795e13f857
Nandini Sharma <nandinisharma050902@gmail.com>
2026-03-16 22:41:45 +0530

    Add files via upload

---
+++ missingnum.java
@@ -0,0 +1,22 @@
+class missingnum {
+    public static int missingNum(int[] arr) {
+        int n = arr.length + 1;
+        for (int i = 1; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main (String[] args) {
+        int[] arr = {8, 2, 4, 5, 3, 7, 1};  
+        System.out.println(missingNum(arr));
+    }
+}
